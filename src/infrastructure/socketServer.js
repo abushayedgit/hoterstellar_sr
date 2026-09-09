0e44bb5ff067cc0937bbd7730b83983b3cc16140
heyAbdullahBruh <mr.sayedf@yandex.com>
2026-09-09 15:50:29 -0400

    del: remove all unnecessary files and codes from this projects

--- src/infrastructure/socketServer.js
+++
@@ -1,8 +0,0 @@
-export function setupSocket(io) {
-  // No-op for now
-  io.on("connection", (socket) => {
-    // Temporary log to confirm socket is working
-    // We'll remove this later
-    console.log("Socket connected");
-  });
-}
